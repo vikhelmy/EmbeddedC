5b421713906d2eddda113bdc6f1510c9da467bc5
MajidHelmy <helmy.ma@outlook.com>
2017-09-28 15:28:27 +0200

    Final Lab 1 and initiating Lab 2

--- Lab1GitHub/Lab1GitHub/main.c
+++ Lab1GitHub/Lab1GitHub/main.c
@@ -10,7 +10,9 @@ void InitialiseGeneral()
 	DDRB = 0xFF;		// Set port B direction OUTPUT (connected to the on-board LEDs)
 	PORTB = 0xFF;		// Set all LEDs initially off (inverted on the board, so '1' = off)
 
-	uCountvalue  = 0;	// Initialize the count value to 0
+	DDRD = 0x00; //It is now input
+	PORTD = 0xFF; //Pull up the buttom
+	uCountvalue  = 1;	// Initialize the count value to 0
 }
 
 int main( void )
@@ -20,16 +22,51 @@ int main( void )
 	while(1)
 	{
 		// Use F9 to place a breakpoint on the line below
-		PORTB = ~uCountvalue;	// Write the data value to Port B (the ~ performs 1s compliment
+		PORTB = uCountvalue;	// Write the data value to Port B (the ~ performs 1s compliment
 		// because the switch values are inverted in the STK300 board hardware)
 		
-		uCountvalue++;			// Increment the count value
+		//uCountvalue++;			// Increment the count value
+		//uCountvalue--; // Decrement the count value
+		
+		if(!(PIND & (1<<PD0))) {
+			if( uCountvalue<9 ) 
+			{
+				uCountvalue = uCountvalue<<1;
+				_delay_ms(2000);
+			}
+		
+		
+			else {
+			
+				uCountvalue=1;
+			}
+		}
+		else 
+			{
+				if(uCountvalue>0 ) 
+					{
+					
+					uCountvalue = uCountvalue>>1;
+					_delay_ms(2000);
+				}
+				
+				else {
+					
+					uCountvalue=8;
+				}
+			}
+		
+		
+		
+		
 
+		
+	
 		// The delay statement below has been commented out because it complicates the debugging process –
 		// especially for beginners
 		// Note that if running the code without debug mode you should un-comment the statement otherwise
 		// the display changes too quickly to see what is happening
 
-		//_delay_ms(500);			// Add a delay so we can see the pattern change
+					// Add a delay so we can see the pattern change
 	}
 }
---
+++ Lab2/Lab2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab2.c
+ *
+ * Created: 9/28/2017 3:28:11 PM
+ * Author : MagedAbdou
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
