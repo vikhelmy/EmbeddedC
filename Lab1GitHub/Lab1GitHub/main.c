9fd2abdd74e8e5161301d171e5fc19e0ab7f26d1
MajidHelmy <helmy.ma@outlook.com>
2017-09-28 11:00:57 +0200

    Import1

---
+++ Lab1GitHub/Lab1GitHub/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab1GitHub.c
+ *
+ * Created: 9/28/2017 10:55:02 AM
+ * Author : MagedAbdou
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
